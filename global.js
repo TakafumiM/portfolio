533bb96b27a6b5926ff74524e21b97fb94f72a4c
TakafumiM <tamatsui@ucsd.edu>
2025-01-23 21:08:33 -0800

    global.js added

---
+++ global.js
@@ -0,0 +1,52 @@
+let pages = [
+    {url: '', title: 'Home'},
+    {url: 'projects/', title: 'Projects'},
+    {url: 'contact/', title: 'Contact'},
+    {url: 'resume/', title: 'Resume'},
+    {url: 'https://github.com/TakafumiM', title: 'GitHub', target: '_blank'}
+  ];
+  
+  let nav = document.createElement('nav');
+  document.body.prepend(nav);
+  
+  for (let p of pages) {
+    let {url, title, target} = p;
+    const ARE_WE_HOME = document.documentElement.classList.contains('home');
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+  
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    if (target) 
+        a.target = target;
+  
+    if (a.host === location.host && a.pathname === location.pathname) {
+      a.classList.add('current');
+    }
+  
+    nav.append(a);
+  }
+  
+  document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <label class="color-scheme">
+      Theme:
+      <select id="theme-switch">
+        <option value="light dark">Automatic</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </label>
+    `
+  );
+  
+  const themeSwitch = document.getElementById('theme-switch');
+  themeSwitch.addEventListener('change', (event) => {
+    const theme = event.target.value;
+    document.documentElement.style.setProperty('color-scheme', theme);
+  });
+
+  
+  
+  
